bdd13b503658e7cec53ee6d2c266662f0c67ca5d
Shayaan02 <shayaan.ahmed@gritacademy.se>
2023-12-13 11:40:31 +0100

    Initial commit

---
+++ src/WeatherApp.java
@@ -0,0 +1,35 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class WeatherApp {
+
+    public static JSONObject getWeatherData (String locationName){
+
+    }
+    public static JSONArray getLocationData(String locationName) {
+
+    }
+
+    private static HttpURLConnection fetchApiResponse(String urlString){
+
+    }
+
+    private static int findIndexOfCurrentTime(JSONArray timeList){
+
+    }
+    private static String getCurrentTime(){
+
+    }
+    private static String convertWeatherCode(long weathercode){
+
+    }
+
+    }
---
+++ src/assets/WeatherAppGui.java
@@ -0,0 +1,25 @@
+package assets;
+import org.json.simple.JSONObject;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+public class WeatherAppGui {
+    public WeatherAppGui(){
+
+    }
+    private void addGuiComponents(){
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    private ImageIcon loadImage(String resourcePath){
+
+    }
+}
